48462e6bdf59f3063bfa2817894195d6939756ea
Dan Zera <dczera@gmail.com>
2017-11-26 21:58:31 -0600

    pull out appropriate data for each <Chart /> component and pass it as props to each

--- src/containers/WeatherList.js
+++ src/containers/WeatherList.js
@@ -6,12 +6,16 @@ import Chart from '../components/Chart';
 class WeatherList extends Component {
 	renderList() {
 		return _.map(this.props.weatherList, cityData => {
+			const tempData = _.map(cityData.list, dataSnapshot => Math.round((dataSnapshot.main.temp * 9 / 5) - 459.67));
+			const pressureData = _.map(cityData.list, dataSnapshot => dataSnapshot.main.pressure);
+			const humidityData = _.map(cityData.list, dataSnapshot => dataSnapshot.main.humidity);
+			console.log('city data', cityData); 
 			return (
 				<tr key={cityData.city.name}>
 					<td>{cityData.city.name}</td>
-					<td><Chart data={cityData} /></td>
-					<td><Chart data={cityData} /></td>
-					<td><Chart data={cityData} /></td>
+					<td><Chart data={tempData} /></td>
+					<td><Chart data={pressureData} /></td>
+					<td><Chart data={humidityData} /></td>
 				</tr>
 			);
 		});
