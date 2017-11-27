1d4594b31b32f021e9445ac9ed8132726f5e5e8a
Dan Zera <dczera@gmail.com>
2017-11-26 21:27:47 -0600

    make <WeatherList /> component an HTML <table>

--- src/containers/WeatherList.js
+++ src/containers/WeatherList.js
@@ -2,11 +2,32 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 class WeatherList extends Component {
+	renderList() {
+		return (
+			<tr>
+				<td>City Here...</td>
+				<td>Chart here...</td>
+				<td>Chart here...</td>
+				<td>Chart here...</td>
+			</tr>
+		);
+	}
+
 	render() {
 		return (
-			<div>
-				WeatherList
-			</div>
+			<table className="table table-hover">
+				<thead>
+					<tr>
+						<th>City</th>
+						<th>Temperature (F)</th>
+						<th>Pressure (hPa)</th>
+						<th>Humidity (%)</th>
+					</tr>
+				</thead>
+				<tbody>
+					{this.renderList()}
+				</tbody>
+			</table>
 		)
 	}
 }
